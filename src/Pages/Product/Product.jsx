b127460a14ed13496d4b2f16850c2a2e29e7437d
Brayan Camilo Bautista Arias <brayanbauti01@gmail.com>
2022-11-07 11:24:46 -0500

    add: request api

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,10 +1,27 @@
 import ImgCarousel from "../../Components/ImgCarousel/ImgCarousel";
 import ProductForm from "../../Components/ProductForm/ProductForm";
+import { useEffect, useState } from "react";
 
-const Product = () => {
+const App = () => {
+  /* STATES */
+  const [response, setResponse] = useState(undefined);
+
+  /* USE EFFECT */
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch(
+        "https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js"
+      );
+      const result = await resp.json();
+      setResponse(result);
+    })();
+  }, []);
+
+  if (response === undefined) return null;
+
   return (
     <>
-      <ImgCarousel />
+      <ImgCarousel imagesArray={response.images} />
       <ProductForm />
     </>
   );
